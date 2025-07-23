0ed30a07ca0e1c5ac206f79efcb0832341918ac3
David Stinson <49245298+DavidStinson@users.noreply.github.com>
2025-07-23 16:53:10 -0500

    edit: align inputs

--- src/hld_builder/main.py
+++ src/hld_builder/main.py
@@ -2,8 +2,6 @@
 import sys
 import warnings
 
-from datetime import datetime
-
 from hld_builder.crew import HldBuilder
 
 warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
@@ -21,48 +19,65 @@ def run():
     # Inputs gathered from:
     # https://docs.google.com/presentation/d/1rVhaBA8aGMDBL7h2xpniqiv_bwVCmri9/edit
 
+    # inputs = {
+    #     'product_title': 'UX for Engineers',
+    #     'client_name': 'M&T Bank',
+    #     'course_delivery_method': 'Instructor-led',
+    #     'course_delivery_modality': 'remote',
+    #     'course_duration_h': 30,
+    #     'course_description': 'This course equips developers with practical UX skills through hands-on exercises with real-world applications. With a strong focus on collaboration, participants will improve communication with UX teams and gain actionable strategies they can use right away.',
+    #     'course_business_outcomes': [
+    #         'Expand the mindset and steps in UX process to entire tech team include low and high fidelity technical design skills and UX research methodologies.',
+    #         'Making developers more self-reliant; speeding up design and development collaboration and process.'
+    #     ],
+    #     'client_priorities': [
+    #         'The course should provide an understanding of baseline research techniques, methodologies and best practices for better cross-discipline collaboration.',
+    #         'Engineers should be able to use tools like Miro to wireframe and communicate ideas and collaborate with the broader team.',
+    #         'Understanding the end-to-end design process will help engineers incorporate UX concepts and approaches into in their decision making and workflow.',
+    #         'Exposure and practice with collaborative tools like the Currency Design System can improve efficiency, velocity and reinforce cohesive product design.'
+    #     ],
+    #     'primary_persona_job_role': 'Software Engineer',
+    #     'learner_persona_description': 'A software engineer with 3+ years of experience in software development, specializing in building web applications using modern JavaScript frameworks and libraries.',
+    #     'learner_persona_attributes_and_background': [
+    #         'Strong foundation in JavaScript, React, and Node.js.',
+    #         'Experience building web applications.',
+    #         'Familiar with modern web development practices and tools.',
+    #         'Strong interest in learning new technologies and approaches.'
+    #     ],
+    #     'learner_persona_existing_knowledge': [
+    #         'Familiar with implementing UI designs using HTML and CSS.',
+    #     ],
+    #     'course_learning_outcomes': [
+    #         'Create polished, professional, and complete designs; and articulate design decisions made to peers and other UX professionals.',
+    #         'Utilize foundational visual design techniques to enhance the overall design of digital products.'
+    #         'Design product interactions with easy-to-use navigation that are logically structured using information architecture methodologies.',
+    #         'Collaborate with a team of fellow designers to solve real-world design challenges using advanced technical design skills.',
+    #         'Evaluate business requirements, metrics, key stakeholders, and technical constraints; and employ product management techniques to design products.'
+    #     ],
+    #     'course_software_and_tools': [
+    #         'Slack',
+    #         'Zoom',
+    #         'Visual Studio Code',
+    #         'Miro',
+    #         'Currency Design System',
+    #     ]
+    # }
+
     inputs = {
-        'product_title': 'UX for Engineers',
-        'client_name': 'M&T Bank',
-        'course_delivery_method': 'Instructor-led',
-        'course_delivery_modality': 'remote',
-        'course_duration_h': 30,
-        'course_description': 'This course equips developers with practical UX skills through hands-on exercises with real-world applications. With a strong focus on collaboration, participants will improve communication with UX teams and gain actionable strategies they can use right away.',
-        'course_business_outcomes': [
-            'Expand the mindset and steps in UX process to entire tech team include low and high fidelity technical design skills and UX research methodologies.',
-            'Making developers more self-reliant; speeding up design and development collaboration and process.'
-        ],
-        'client_priorities': [
-            'The course should provide an understanding of baseline research techniques, methodologies and best practices for better cross-discipline collaboration.',
-            'Engineers should be able to use tools like Miro to wireframe and communicate ideas and collaborate with the broader team.',
-            'Understanding the end-to-end design process will help engineers incorporate UX concepts and approaches into in their decision making and workflow.',
-            'Exposure and practice with collaborative tools like the Currency Design System can improve efficiency, velocity and reinforce cohesive product design.'
-        ],
-        'primary_persona_job_role': 'Software Engineer',
-        'learner_persona_description': 'A software engineer with 3+ years of experience in software development, specializing in building web applications using modern JavaScript frameworks and libraries.',
-        'learner_persona_attributes_and_background': [
-            'Strong foundation in JavaScript, React, and Node.js.',
-            'Experience building web applications.',
-            'Familiar with modern web development practices and tools.',
-            'Strong interest in learning new technologies and approaches.'
-        ],
-        'learner_persona_existing_knowledge': [
-            'Familiar with implementing UI designs using HTML and CSS.',
-        ],
-        'course_learning_outcomes': [
-            'Create polished, professional, and complete designs; and articulate design decisions made to peers and other UX professionals.',
-            'Utilize foundational visual design techniques to enhance the overall design of digital products.'
-            'Design product interactions with easy-to-use navigation that are logically structured using information architecture methodologies.',
-            'Collaborate with a team of fellow designers to solve real-world design challenges using advanced technical design skills.',
-            'Evaluate business requirements, metrics, key stakeholders, and technical constraints; and employ product management techniques to design products.'
-        ],
-        'course_software_and_tools': [
-            'Slack',
-            'Zoom',
-            'Visual Studio Code',
-            'Miro',
-            'Currency Design System',
-        ]
+        'product_title': "",
+        'client_name': "",
+        'course_delivery_method': "",
+        'course_delivery_modality': "",
+        'course_duration_h': 0,
+        'course_description': "",
+        'course_business_outcomes': [],
+        'client_priorities': [],
+        'primary_persona_job_role': "",
+        'learner_persona_description': "",
+        'learner_persona_attributes_and_background': [],
+        'learner_persona_existing_knowledge': [],
+        'course_learning_outcomes': [],
+        'course_software_and_tools': []
     }
     
     try:
@@ -76,7 +91,20 @@ def train():
     Train the crew for a given number of iterations.
     """
     inputs = {
-        "topic": "AI LLMs"
+        'product_title': "",
+        'client_name': "",
+        'course_delivery_method': "",
+        'course_delivery_modality': "",
+        'course_duration_h': 0,
+        'course_description': "",
+        'course_business_outcomes': [],
+        'client_priorities': [],
+        'primary_persona_job_role': "",
+        'learner_persona_description': "",
+        'learner_persona_attributes_and_background': [],
+        'learner_persona_existing_knowledge': [],
+        'course_learning_outcomes': [],
+        'course_software_and_tools': []
     }
     try:
         HldBuilder().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
@@ -99,8 +127,20 @@ def test():
     Test the crew execution and returns the results.
     """
     inputs = {
-        "topic": "AI LLMs",
-        "current_year": str(datetime.now().year)
+        'product_title': "",
+        'client_name': "",
+        'course_delivery_method': "",
+        'course_delivery_modality': "",
+        'course_duration_h': 0,
+        'course_description': "",
+        'course_business_outcomes': [],
+        'client_priorities': [],
+        'primary_persona_job_role': "",
+        'learner_persona_description': "",
+        'learner_persona_attributes_and_background': [],
+        'learner_persona_existing_knowledge': [],
+        'course_learning_outcomes': [],
+        'course_software_and_tools': []
     }
     try:
         HldBuilder().crew().test(n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs)
